54d3623ff3af83727f5fa198ab436ee8bfe1ddca
adamaho <aho.adam01@gmail.com>
2023-07-26 19:11:30 -0400

    adding update

--- muma-db/src/restaurant.rs
+++ muma-db/src/restaurant.rs
@@ -1,5 +1,6 @@
 use sea_orm::entity::prelude::*;
 use sea_orm::IntoActiveModel;
+use sea_orm::Set;
 use serde::{Deserialize, Serialize};
 
 //////////////////////////////////////////////////////////////////
@@ -68,4 +69,44 @@ pub async fn insert(
     new_restaurant: InsertRestaurant,
 ) -> anyhow::Result<Model, DbErr> {
     new_restaurant.into_active_model().insert(conn).await
+}
+
+#[derive(Debug, Deserialize, DeriveIntoActiveModel)]
+pub struct UpdateRestaurant {
+    pub name: String,
+    pub address_line1: Option<String>,
+    pub address_line2: Option<String>,
+    pub city: Option<String>,
+    pub country: Option<String>,
+    pub postal_code: Option<String>,
+    pub state: Option<String>,
+}
+
+pub async fn update(
+    conn: &impl ConnectionTrait,
+    restaurant_id: i32,
+    restaurant: UpdateRestaurant,
+) -> anyhow::Result<Model, DbErr> {
+    let r: ActiveModel = Entity::find_by_id(restaurant_id)
+        .one(conn)
+        .await?
+        .ok_or(DbErr::Custom(format!(
+            "Could not find restaurant with id: {}",
+            restaurant_id.clone(),
+        )))
+        .map(Into::into)?;
+
+    ActiveModel {
+        restaurant_id: r.restaurant_id,
+        name: Set(restaurant.name),
+        address_line1: Set(restaurant.address_line1),
+        address_line2: Set(restaurant.address_line2),
+        city: Set(restaurant.city),
+        country: Set(restaurant.country),
+        postal_code: Set(restaurant.postal_code),
+        state: Set(restaurant.state),
+        created_at: r.created_at,
+    }
+    .update(conn)
+    .await
 }
