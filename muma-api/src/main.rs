1b2576b235a5c66a1521e0488731a1e3b197a199
adamaho <aho.adam01@gmail.com>
2023-08-02 19:42:44 -0400

    (feat): installing and configuring actix with cors and ssl

--- muma-api/src/main.rs
+++ muma-api/src/main.rs
@@ -1,21 +1,37 @@
-use muma_db::user;
-use muma_db::Database;
+use actix_cors::Cors;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
-    let pool = Database::from_env_path(".env")?.connect().await?;
+/// Handle the index request
+async fn ping(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().body("pong")
+}
 
-    let user_id = user::insert_user(
-        &pool,
-        user::UserForm {
-            username: String::from("adamaho77"),
-        },
-    )
-    .await?
-    .try_into()?;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
 
-    let user = user::select_by_id(&pool, user_id).await?;
+    builder
+        .set_private_key_file("./muma-api/key.pem", SslFiletype::PEM)
+        .unwrap();
 
-    println!("{:?}", user);
-    Ok(())
+    builder
+        .set_certificate_chain_file("./muma-api/cert.pem")
+        .unwrap();
+
+    println!("Server started at https://localhost:3001");
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allow_any_method()
+            .allow_any_header()
+            .supports_credentials()
+            .max_age(3600);
+
+        App::new().wrap(cors).route("/ping", web::get().to(ping))
+    })
+    .bind_openssl("localhost:3001", builder)?
+    .run()
+    .await
 }
